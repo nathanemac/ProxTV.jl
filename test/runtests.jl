769f86237a022572ff305b07deab22359b971dbe
nathanemac <91251698+nathanemac@users.noreply.github.com>
2024-09-13 15:03:09 -0400

    add tests and CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using ProxTV
 using Test
+using LinearAlgebra
 
 @testset "ProxTV.jl" begin
-  @test ProxTV.hello_world() == "Hello, World!"
+  # test on a basic function :
+  n = 4
+  x = rand(n)
+  p = 2.0
+  @test isapprox(ProxTV.LPnorm(x, n, p), norm(x), atol = 1e-5)
+
+  # test on a more advanced function :
+  lambda = 0.18
+  info = []
+  y = rand(n)
+  x = zeros(n)
+  ws = ProxTV.newWorkspace(n)
+  @test ProxTV.TV(y, lambda, x, info, n, p, ws) == 1 # 1 is the expected return value of the function
+
 end
